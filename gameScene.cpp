2ea38f574c2bfb56a3726bc09fa35ace35260e8a
ReliefAmbient <shadowbutterfly1925@gmail.com>
2020-10-16 09:29:33 +0900

    SkinnedCube実装

--- gameScene.cpp
+++ gameScene.cpp
@@ -11,7 +11,10 @@
 #include "ground_blocks.h"
 #include "map_data.h"
 
+#include "SkinndeCube.h"
+
 #include <memory>
+#include <DirectXMath.h>
 
 
 MyMesh field;
@@ -20,6 +23,8 @@ Camera camera;
 Player  player;
 GroundBlockManager block;
 
+SkinndeCube* scube;
+FileName cube_texture[5];
 
 void SceneGame::Initialize()
 {
@@ -42,6 +47,14 @@ void SceneGame::Initialize()
     sky.Initialize();
     sky.staticLoad(L"./Data/Sky/sky.obj", "not_light");
     sky.scale = VECTOR3(30.0f, 30.0f, 30.0f);
+
+    //” 
+    cube_texture[0] = L"./Data/1.jpg";
+    cube_texture[1] = L"./Data/Floor/Floor0.png";
+    cube_texture[2] = L"./Data/Floor/Floor1.png";
+    cube_texture[3] = L"./Data/Floor/Floor2.png";
+    cube_texture[4] = L"./Data/Floor/Floor3.png";
+    scube = new SkinndeCube(device, cube_texture, 5);
 
     player.Initialize(new GeometricCube(pFramework->getDevice()));
    // player.Initialize(new GeometricCube(device));
@@ -71,10 +84,10 @@ void SceneGame::Render(float elapsedTime)
     ID3D11DeviceContext* context = pFramework->getDeviceContext();
 
     static bool wireframe = false;
-    if (GetAsyncKeyState(' ') & 1)
-    {
-        wireframe = !wireframe;
-    }
+    //if (GetAsyncKeyState(' ') & 1)
+    //{
+    //    wireframe = !wireframe;
+    //}
     static int anime = 0;
     if (GetAsyncKeyState('X') & 1)
     {
@@ -91,7 +104,42 @@ void SceneGame::Render(float elapsedTime)
     player.Render(view, projection, lightDirection, wireframe);
     sky.Render(view, projection, lightDirection, wireframe);
 
-    block.Render(context, view, projection, lightDirection, wireframe);
+    //block.Render(context, view, projection, lightDirection, wireframe);
+
+    //” 
+    DirectX::XMMATRIX world;
+    DirectX::XMMATRIX Scale, Rotation, Rx, Ry, Rz, Translation;
+
+    //	‰Šú‰»
+    world = DirectX::XMMatrixIdentity();
+    //	Šg‘åEk¬
+    Scale = DirectX::XMMatrixScaling(1, 0.5, 1);
+
+    //	‰ñ“]
+    Rx = DirectX::XMMatrixRotationX(0);				//	XŽ²‚ðŠî€‚Æ‚µ‚½‰ñ“]s—ñ
+    Ry = DirectX::XMMatrixRotationY(0);				//	YŽ²‚ðŠî€‚Æ‚µ‚½‰ñ“]s—ñ
+    Rz = DirectX::XMMatrixRotationZ(0);				//	ZŽ²‚ðŠî€‚Æ‚µ‚½‰ñ“]s—ñ
+    Rotation = Rz * Ry * Rx;
+
+    //	•½sˆÚ“®
+    Translation = DirectX::XMMatrixTranslation(-10,0,0);
+
+    //	ƒ[ƒ‹ƒh•ÏŠ·s—ñ
+    world = Scale * Rotation * Translation;
+
+    DirectX::XMFLOAT4X4 wvp;
+    DirectX::XMFLOAT4X4 W;
+    XMStoreFloat4x4(&wvp, (world * view * projection));
+    XMStoreFloat4x4(&W, world);
+
+    static int FloorNUM = 1;
+    if (GetAsyncKeyState('N') & 1)
+    {
+        FloorNUM++;
+    }
+    if (FloorNUM > 4) { FloorNUM = 1; }
+
+    scube->Render(context, FloorNUM, wvp, W);
     pPause->Draw();
 }
 
